2bd02988576e716d722873ff9d7a1ddfd80686fc
AngelJMavare <angeljmavare@gmail.com>
2024-11-16 12:08:26 -0400

    Feat: hack_4

--- hack_4.py
+++ hack_4.py
@@ -6,8 +6,10 @@ text: "barziman" output => "arzima"
 text: "qux" output => "qux" 
 """
 
-
 def fn_hack_4(s):
     result = s
-    #...
-    return result
+    if len(result) > 3:
+        result = result[1:-1]
+        return result
+    else:
+        return result
