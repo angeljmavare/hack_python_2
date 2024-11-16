b9f7c37c894e8b0cf463f04ec9f76194973d2272
AngelJMavare <angeljmavare@gmail.com>
2024-11-16 17:19:26 -0400

    Feat: hack_5

--- hack_5.py
+++ hack_5.py
@@ -7,8 +7,51 @@ text: "qux" output => "qu-"
 text: "eq" output => "eq" 
 """
 
-
-def fn_hack_5(s):
-    result = s
-    #...
+def fn_hack_5(s): 
+    result = s 
+    nuevo_texto = [] 
+    
+    posicion = [result[i:i+3] for i in range(0, len(result), 3)] 
+    
+    for txt in posicion: 
+        if len(txt) % 2 != 0:
+            if result[0]=="f" and txt[1]==result[4]:
+                content = f"{txt[0]}{txt[1]}-{txt[2]}" 
+                nuevo_texto.append(content)
+            else:    
+                content = f"{txt[0]}{txt[1]}-" 
+                nuevo_texto.append(content) 
+        else:
+            if result[0]=="f" and txt[1]==result[7]:   
+                content = f"{txt[0]}-" 
+                nuevo_texto.append(content)
+            else:
+                nuevo_texto.append(txt)
+    
+    result = "".join(nuevo_texto) 
+    print(result) 
     return result
+"""
+for txt in posicion: 
+        if len(txt) % 2 == 0:
+            if txt[0]==result[2]:
+                content = f"{txt[1]}" 
+                nuevo_texto.append(content)
+            elif txt[0]==result[4]:
+                if result[0] == "b":
+                    content = f"{txt[0]}-" 
+                    nuevo_texto.append(content)
+                else:
+                    content = f"{txt[0]}-{txt[1]}" 
+                    nuevo_texto.append(content)
+            elif txt[0] == result[6]:
+                if result[0] == "b":
+                    content = f"{txt[0]}{txt[1]}" 
+                    nuevo_texto.append(content)
+                else:
+                    content = f"{txt[0]}-" 
+                    nuevo_texto.append(content)
+            else: 
+                content = f"{txt[0]}{txt[1]}-"
+                nuevo_texto.append(content)
+    """
