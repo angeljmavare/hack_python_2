74f59c5357c57a053124dfcc27f3a3ee54c4b7e5
AngelJMavare <angeljmavare@gmail.com>
2024-11-16 18:07:14 -0400

    Feat: hack_6

--- hack_6.py
+++ hack_6.py
@@ -8,8 +8,17 @@ text: ["a","b","c","d","e"] output => ["1","-","3","-","5"]
 text: [] output => ["0"] 
 """
 
-
 def fn_hack_6(s):
     result = s
-    #...
-    return result
+    salida= []
+    
+    for i in range(len(result)):
+        if i % 2 == 0:
+            salida.append(str(i+1))
+        else:
+            salida.append("-")
+        
+    if not result:
+        salida.append("0")
+    
+    return salida
