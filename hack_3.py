987907160520be2164105bacd42a8b8279d7f840
AngelJMavare <angeljmavare@gmail.com>
2024-11-16 12:03:23 -0400

    Feat: hack_3

--- hack_3.py
+++ hack_3.py
@@ -14,8 +14,10 @@ text: "qu" output => "Qv"
 text: "qux" output => "QvX" 
 """
 
-
 def fn_hack_3(s):
     result = s
-    #...
+    result = result.capitalize()
+    result = result[:-1] + result[-1].upper()
+    result = result.replace("a","@").replace("e","3").replace("i","¡").replace("o","0").replace("u","v").replace("U","v")
     return result
+  
