fd325254604f709ef35c6aa141a79f4ac13b50a9
AngelJMavare <angeljmavare@gmail.com>
2024-11-16 18:31:58 -0400

    Feat: Hack_7

--- hack_7.py
+++ hack_7.py
@@ -8,8 +8,19 @@ text: ["a","b","c","d","e"] output => ["1",2,"3",4,"5"]
 text: [0] output => [0] 
 """
 
-
 def fn_hack_7(s):
     result = s
-    #...
-    return result
+    salida= []
+    
+    i=0
+    while i < len(result):
+        if i % 2 == 0:
+            salida.append(str(i+1))
+        else:
+           salida.append(i+1)
+        i+=1  
+      
+    if len(result) == 1:
+        return result
+    
+    return salida
