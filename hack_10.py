cb5e01724a3cee08693ea18de61f041d34222a68
AngelJMavare <angeljmavare@gmail.com>
2024-11-16 21:30:13 -0400

    Feat: hack_10

--- hack_10.py
+++ hack_10.py
@@ -5,5 +5,14 @@ text: [{"a":"b"},{"c","d"},{"e":"f"}] output => [{"1":"2"},{"3","4"},{"5":"6"}]
 
 def fn_hack_10(s):
     result = s
-    #...
-    return result
+    lista = []
+    contador_clave= 1
+    contador_llave= 2
+    
+    for dic in result:
+        nuevo_diccionario= {}
+        nuevo_diccionario[str(contador_clave)]= str(contador_llave)
+        contador_clave+=2
+        contador_llave+=2
+        lista.append(nuevo_diccionario)  
+    return lista
