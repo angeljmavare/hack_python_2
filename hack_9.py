a04d14727b0b525bbd5512d8266784033f504d39
AngelJMavare <angeljmavare@gmail.com>
2024-11-16 21:02:46 -0400

    Feat: hack_9

--- hack_9.py
+++ hack_9.py
@@ -3,6 +3,12 @@ text: {"foo":"fookziman","bar":"barziman"} output => {"Foo":"Fooziman"}
 """
 
 def fn_hack_9(s):
-    result = s
-    result ={"Foo":"Fooziman"}
-    return result
+    s["Foo"]="Fooziman"
+    print (s)
+    
+    resultado ={}
+    
+    for clave, valor in s.items():
+        if clave == "Foo":
+            resultado[clave]= valor
+    return resultado
